3ddf6488315815ffa33a528c1f8b3c7575c3a9f3
NIKHIL PRAKASH <nikhilprakash510@gmail.com>
2020-07-22 11:47:59 +0530

    Add files via upload

--- LibraryManagementApp/src/main/java/com/example/Library/App/LibraryManagementApp.java
+++ LibraryManagementApp/src/main/java/com/example/Library/App/LibraryManagementApp.java
@@ -2,8 +2,12 @@ package com.example.Library.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+
+//visit for documentation-> http://localhost:8080/swagger-ui.html#/
 public class LibraryManagementApp {
 
 	public static void main(String[] args) {
